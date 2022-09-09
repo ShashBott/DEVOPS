e74c73eeaa689467ab37e2371d37cef347bdf4a7
nitigautam <112678723+nitigautam@users.noreply.github.com>
2022-09-09 09:36:36 +0530

    Update PairCoding.java

--- PairCoding.java
+++ PairCoding.java
@@ -1,1 +1,13 @@
+import java.util.*;
+class PairCoding
+{
+    static void SI()
+    {
+        Scanner sc=new Scanner(System.in);
+        int P = sc.nextInt();
+        int R = sc.nextInt();
+        int T = sc.nextInt();
+        float SimpleInterest = (P*R*T) / 100;
 
+        System.out.println(SimpleInterest);
+    }
